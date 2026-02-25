d8d78ebbbc24e16f806cd3a014ac6da4194d7240
lim9292 <dobby630@gmail.com>
2026-02-25 10:06:39 +0900

    function

--- es6.js
+++ es6.js
@@ -6,3 +6,60 @@ const greeting = (username = "사용자") => {
 
 greeting();
 greeting("임은혜");
+
+// 논리연산자 (And, Or, Not)
+const user = {
+  isLoggedIn: true, // 로그인 여부
+  role: "admin", // 다른 값으로 "user", "guest"
+};
+
+// 관리자 페이지 접근 코드
+if (user.isLoggedIn && user.role === "admin") {
+  // login 상태이면서 role이 admin인 경우 실행
+  console.log("✅관리자 페이지에 접근할 수 있습니다.");
+} else {
+  console.log("⚠️관리자 페이지에 접근할 수 없습니다.");
+}
+
+const person = {
+  age: 17,
+  isParentPermission: false, // 부모님 허락 여부
+};
+
+const canAccess = person.age >= 18 || person.isParentPermission;
+console.log("🚀 ~ canAccess:", canAccess);
+
+if (canAccess) {
+  // 성인이거나, 부모님의 허락을 받은 경우
+  console.log("✅접근 가능");
+} else {
+  console.log("⚠️접근 불가");
+}
+
+const adult = person.age >= 18 && "성인";
+console.log("🚀 ~ adult:", adult);
+
+// 구조분해할당
+const colors = ["red", "blue", "green"];
+// const first = color[0];
+// const second = color[1];
+
+const [second, first] = colors;
+console.log("🚀 ~ first:", first);
+console.log("🚀 ~ second:", second);
+
+const todoItem = {
+  id: 123,
+  todo: "React학습",
+  isComplete: false,
+};
+
+// const id = todoItem.id;
+// const todo = todoItem.todo;
+const isComplete = "🙃🙃🙃메롱🙃🙃🙃";
+
+const { todo, id, isComplete: todoIsComplete } = todoItem;
+
+console.log("🚀 ~ todoItem.id:", todoItem.id);
+console.log("🚀 ~ todoItem.todo:", todoItem.todo);
+console.log("🚀 ~ todoItem.isComplete:", todoItem.isComplete);
--- quiz.js
+++ quiz.js
@@ -4,13 +4,17 @@ function add(a, b) {
 }
 */
 
-const add = (a, b) => a + b;
-console.log(add(5, 3)); // 예상 결과: 8
+const add = (a, b) => {
+  return a + b;
+};
+console.log("A1: " + add(5, 3)); // 예상 결과: 8
 
 /* Q2. 두 개의 숫자를 입력받아 그 차이를 계산하는 함수를 화살표 함수로 작성하세요.*/
 
-const minus = (a, b) => a - b;
-console.log(minus(60, 20)); // 예상 결과: 40
+const minus = (a, b) => {
+  return a - b;
+};
+console.log("A2: " + minus(60, 20)); // 예상 결과: 40
 
 /* Q3. 다음 함수가 주어졌을 때, 이 함수를 호출하여 "안녕하세요, [이름]님. 당신의 나이는 [나이]세입니다."라는 메시지를 출력하는 코드를 작성하세요.*/
 
@@ -18,28 +22,30 @@ const introduce = (name, age) => {
   return "안녕하세요, " + name + "님. 당신의 나이는 " + age + "세입니다.";
 };
 
-console.log(introduce("김철수", 25)); // 예시 결과: "안녕하세요, 김철수님. 당신의 나이는 25세입니다."
+console.log("A3: " + introduce("김철수", 25)); // 예시 결과: "안녕하세요, 김철수님. 당신의 나이는 25세입니다."
 
 /* Q4. 샌드위치 주문 함수를 작성하세요. 아래 콘솔 출력 결과를 보고 orderSandwich 함수를 화살표 함수로 구현하세요. */
 //orderSandwich 함수는 type 매개변수를 받고 타입에 따라 다른 재료를 반환해야 합니다
 
+const orderSandwich = (type = "basic") =>
+  type === "vegan" ? "🥖 + 🥬 + 🍅 + 🥑 + 🥒" : "🥖 + 🥬 + 🍅 + 🧀 + 🥓";
+
+/*
 const orderSandwich = (type) => {
-  if (type == "basic") {
-    console.log("case 1");
+  if (type === "basic") {
     return "🥖 + 🥬 + 🍅 + 🧀 + 🥓";
-  } else if (type == "vegan") {
-    console.log("case 2");
+  } else if (type === "vegan") {
     return "🥖 + 🥬 + 🍅 + 🥑 + 🥒";
   } else {
-    console.log("case 3");
     return "🥖 + 🥬 + 🍅 + 🧀 + 🥓";
   }
 };
+*/
 
 // 함수 테스트 - 아래 코드는 수정하지 마세요
-console.log("기본 샌드위치 주문: " + orderSandwich());
-console.log("기본 샌드위치 주문: " + orderSandwich("basic"));
-console.log("비건 샌드위치 주문: " + orderSandwich("vegan"));
+console.log("A4-1. 기본 샌드위치 주문: " + orderSandwich());
+console.log("A4-2. 기본 샌드위치 주문: " + orderSandwich("basic"));
+console.log("A4-3. 비건 샌드위치 주문: " + orderSandwich("vegan"));
 
 /* 예상 결과:
 기본 샌드위치 주문: 🥖 + 🥬 + 🍅 + 🧀 + 🥓
@@ -55,14 +61,14 @@ console.log("예상 출력: 5 4 3 2 1");
 
 /* Q6. 다음 코드에서 각 숫자에 1을 더한 새로운 배열을 만들어 주세요.(map) */
 const numbers = [1, 2, 3, 4, 5];
-const incrementedNumbers = numbers.map((numbers) => numbers + 1);
-console.log(incrementedNumbers);
+const incrementedNumbers = numbers.map((number) => number + 1);
+console.log("A6: " + incrementedNumbers);
 // 예상 결과: [2, 3, 4, 5, 6]
 
 /* Q7. 다음 코드에서 짝수만 필터링된 새로운 배열을 만들어 주세요 (filter) */
 const numbers2 = [1, 2, 3, 4, 5, 6];
-const evenNumbers = numbers2.filter((numbers2) => numbers2 % 2 == 0);
-console.log(evenNumbers); // 예상 결과: [2, 4, 6]
+const evenNumbers = numbers2.filter((number) => number % 2 === 0);
+console.log("A7: " + evenNumbers); // 예상 결과: [2, 4, 6]
 
 /* Q8. 다음 코드에서 각 문제를 풀어보세요. (`find`) */
 
@@ -76,9 +82,9 @@ const students = [
 ];
 
 // 1. 전공이 "Physics"인 학생을 찾으세요.
-const physicsStudent = students.find((students) => students.major == "Physics");
-console.log("전공이 Physics인 학생:", physicsStudent); // 예상 결과: { name: "Charlie", age: 23, major: "Physics" },
+const physicsStudent = students.find((student) => student.major === "Physics");
+console.log("A8-1: 전공이 Physics인 학생:", physicsStudent); // 예상 결과: { name: "Charlie", age: 23, major: "Physics" },
 
 // 2. 이름이 "David"인 학생을 찾으세요.
-const davidStudent = students.find((students) => students.name == "David"); // 여기에 코드를 작성하세요.
-console.log("이름이 David인 학생:", davidStudent); // 예상 결과:{ name: "David", age: 20, major: "Chemistry" },
+const davidStudent = students.find((student) => student.name === "David"); // 여기에 코드를 작성하세요.
+console.log("A8-2: 이름이 David인 학생:", davidStudent); // 예상 결과:{ name: "David", age: 20, major: "Chemistry" },
